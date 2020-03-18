720e34e79fb1edb3623509bd8b7f7acfb9a73560
emil nordin <emnordi@gmail.com>
2020-03-18 18:01:36 +0000

    heart disease

--- app.py
+++ app.py
@@ -43,18 +43,26 @@ def my_form():
 
 @app.route('/', methods=['POST', 'GET'])
 def my_form_post():
-    price = request.form['price']
-    Tax = request.form['Tax']
-    Driver_Age = request.form['Driver_Age']
-    Licence_Length_Years = request.form['Licence_Length_Years']
-    req_model = request.form['req_model']
+    age = request.form['age']
+    sex = request.form['sex']
+    cp = request.form['cp']
+    testbps = request.form['testbps']
+    chol = request.form['chol']
+    fbs = request.form['fbs']
+    restecg = request.form['restecg']
+    thalach = request.form['thalach']
+    exang = request.form['exang']
+    oldpeak = request.form['oldpeak']
+    slope = request.form['slope']
+    ca = request.form['ca']
+    thal = request.form['thal']
 
-    target = get_predictions(price, Tax, Driver_Age, Licence_Length_Years, req_model)
+    target = get_predictions(age, sex, cp, testbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal)
 
     if target==1:
-        sale_making = 'Customer is likely to buy the insurance'
+        sale_making = 'Person is likely to have heart disease'
     else:
-        sale_making = 'Customer is unlikely to buy the insurance'
+        sale_making = 'Person is unlikely to have heart disease'
 
     return render_template('home.html', target = target, sale_making = sale_making)
 
